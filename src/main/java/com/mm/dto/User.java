8b756f87e20fec55d33e8dff71e75305d19df53f
miaokaicheng <ex-miukaicheng001@pingan.com.cn>
2021-12-28 14:45:59 +0800

    修改实体类接收Date格式解析异常问题

--- src/main/java/com/mm/dto/User.java
+++ src/main/java/com/mm/dto/User.java
@@ -1,6 +1,7 @@
 package com.mm.dto;
 
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 
@@ -14,5 +15,6 @@ public class User {
     private int id;
     private String userName;
     private int sex;
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date birthday;
 }
